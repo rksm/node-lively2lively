7d95c256af2f1a77a79f3fa3e1dce79f6dcb51f2
Robert Krahn <robert.krahn@gmail.com>
2014-03-19 21:30:57 -0700

    updated browserified script

--- lively2lively-browserified.js
+++ lively2lively-browserified.js
@@ -4,7 +4,10 @@ var Connection = require('./lib/browser-client').Connection,
     services   = require('./lib/default-services'),
     lv         = require('./lib/browser-lively-interface').noConflict('lv');
 
-var l2l = {
+var l2l = lv.l2l || {
+
+    session: null,
+
     connect: function connect(options, thenDo) {
         options = options || {};
     
@@ -30,8 +33,8 @@ var l2l = {
     },
 
     disconnect: function() {
-        var s = lv.l2l.session;
-        if (s) { s.unregister(); lv.l2l.session = null; }
+        var s = l2l.session;
+        if (s) { s.unregister(); l2l.session = null; }
     }
 }
 
@@ -40,22 +43,42 @@ var l2l = {
 function optionsFromScript() {
     var scriptName = "lively2lively-browserified.js",
         options = {},
-        scripts = document.getElementsByTagName('script');
+        scripts = document.getElementsByTagName('script'),
+        l2lScripts = [],
+        script;
+
     for (var i = 0; i < scripts.length; i++) {
         var src = scripts[i].src;
-        if (src.indexOf(scriptName) === -1) continue;
-        var optionsMatch = src.match(/\?(.*)/);
-        if (!optionsMatch || !optionsMatch[1]) break;
-        optionsMatch[1].split('&').forEach(function(opt) {
-            var kv = opt.split('=');
-            options[kv[0]] = kv[1] && kv[1] === 'false' ? false : kv[1];
-        });
-        break;
+        if (src.indexOf(scriptName) !== -1) l2lScripts.push(scripts[i]);
     }
+
+    if (l2lScripts.length === 0) {
+        console.warn('Could not find lively2lively script file for option extraction.');
+    } else if (l2lScripts.length >= 2) {
+        console.warn('Found multiple lively2lively script files when extracting options. Using the last one.');
+        script = l2lScripts[l2lScripts.length-1];
+    } else { script = l2lScripts[0]; }
+
+    if (!script) return options;
+
+    var optionsMatch = script.src.match(/\?(.*)/);
+    if (!optionsMatch || !optionsMatch[1]) return options;
+    optionsMatch[1].split('&').forEach(function(opt) {
+        var kv = opt.split('=');
+        options[kv[0]] = kv[1] && kv[1] === 'false' ? false : kv[1];
+    });
+
     return options;
 }
 
+function dealWithExistingL2lSession() {
+    if (!l2l.session || !l2l.session.isConnected()) return;
+    console.warn("Found exisiting l2l connection. Deactivating it...");
+    lv.l2l.disconnect();
+}
+
 (function autoload() {
+    dealWithExistingL2lSession();
     var options = optionsFromScript();
     if (options.autoload === false) return;
     l2l.connect(options, function(err, session) {
