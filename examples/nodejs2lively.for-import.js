90e01f3f8268493ede6cc519d6804190f1ad37ab
Robert Krahn <robert.krahn@gmail.com>
2014-03-19 21:25:33 -0700

    simple nodejs import example

---
+++ examples/nodejs2lively.for-import.js
@@ -0,0 +1,35 @@
+// This example code is a ready made nodejs l2l connect. Just require this file,
+// it will auto connect and register to the supplied l2l server behind baseURL.
+// You can then send messages to and from the nodejs process.
+
+var options = {
+    baseURL: process.env.L2L_SERVERURL || 'http://lively-web.org:8080',
+    name: process.env.L2L_CLIENTNAME || 'l2l-from-nodejs'
+};
+
+var services = require("../lib/default-services");
+var connectedSession;
+
+require('../nodejs-connect')(options, function(err, session) {
+    if (err) { console.error(err); return; }
+    console.log("Connected via %s", session);
+
+    connectedSession = session;
+
+    // message dispatch
+    session.on('lively-message', function(msg, connection) {
+        if (msg.action in services) services[msg.action](msg, session);
+        else services.messageNotUnderstood(msg, session);
+    });
+
+    // register session so it can receive messages
+    session.register();
+
+    process.on('exit', function(code) { // cleanup
+        if (session && session.isOpen())  session.unregister();
+    });
+});
+
+module.exports = {
+    get session() { return connectedSession; }
+}
